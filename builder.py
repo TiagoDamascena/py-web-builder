10ec58405a9f1f3a00b4e939c7ae52a4e4552941
Tiago Damascena <tiagobdamascena@gmail.com>
2018-06-16 19:15:30 -0300

    Builder rewriten to an import-based model

--- builder.py
+++ builder.py
@@ -4,13 +4,11 @@ import os
 import shutil
 import re
 import markdown2
-import copy
 
 color_green = '\033[0;32m{0}\033[0;0m'
 color_red = '\033[1;31m{0}\033[0;0m'
 
 class WebsiteBuilder:
-
     def __init__(self):
         # Change working directory to current directory
         working_directory = os.getcwd()
@@ -18,7 +16,6 @@ class WebsiteBuilder:
 
         # Load the configuration file
         self.config = Config()
-        self.config.parse_file()
 
     def build(self):
         # Destroy old destination folder
@@ -30,7 +27,7 @@ class WebsiteBuilder:
         # Copy files
         print 'Copying files...'
         for copy in self.config.copies:
-            copy.copy(self.config.build_folder)
+            copy.copy()
 
         # Build files
         print 'Building files...'
@@ -38,147 +35,147 @@ class WebsiteBuilder:
             build.build()
 
 class Config:
-    config = []
-    build_folder = 'dist/'
-    start_tag = '<!--'
-    end_tag = '-->'
-    copies = []
-    builds = []
-
     def __init__(self):
+        self.file_name = 'web-builder.json'
+
+        self.build_folder = 'dist/'
+        self.copies = []
+        self.builds = []
+
+        file_content = self.load_config()
+        self.parse_file(file_content)
+
+    def load_config(self):
         print 'Loading configuration file...'
-        if os.path.exists('builder.json'):
-            self.config = json.load(open('builder.json', 'r'))
+        if os.path.exists(self.file_name):
+            file_content = json.load(open(self.file_name, 'r'))
             print color_green.format('Configuration file loaded')
+            return file_content
         else:
-            print color_red.format('Configuration file not found, make sure you are in the correct folder')
+            print color_red.format('Configuration file not found, make sure you are in the correct directory')
             exit()
 
-    def parse_file(self):
-        if 'config' in self.config:
-            self.parse_config(self.config['config'])
-        if 'copy' in self.config:
-            self.parse_copy(self.config['copy'])
-        if 'build' in self.config:
-            self.parse_build(self.config['build'])
+    def parse_file(self, file_content):
+        if 'config' in file_content:
+            self.parse_config(file_content['config'])
+        if 'copy' in file_content:
+            self.parse_copy(file_content['copy'])
+        if 'build' in file_content:
+            self.parse_build(file_content['build'])
 
     def parse_config(self, configs):
         if 'build_folder' in configs:
             self.build_folder = configs['build_folder']
-        if 'start_tag' in configs:
-            self.start_tag = configs['start_tag']
-        if 'end_tag' in configs:
-            self.end_tag = configs['end_tag']
 
     def parse_copy(self, copies):
-        for copy in copies:
-            self.copies.append(Copy(copy))
+        for path in copies:
+            self.copies.append(Copy(self, path))
 
     def parse_build(self, builds):
-        for build in builds:
-            self.builds.append(Build(self, ResultFile(self), build))
+        for path in builds:
+            self.builds.append(Build(self, path))
 
 class Copy:
+    def __init__(self, config, path):
+        self.config = config
+        self.path = path
 
-    def __init__(self, copy):
-        self.path_from = copy['from']
-        self.path_to = copy['to']
-
-    def copy(self, build_folder):
-        if os.path.exists(self.path_from):
-            if os.path.isdir(self.path_from):
-                shutil.copytree(self.path_from, build_folder + self.path_to)
-                print color_green.format('Folder ' + self.path_from + ' copied')
+    def copy(self):
+        if os.path.exists(self.path):
+            if os.path.isdir(self.path):
+                shutil.copytree(self.path, os.path.join(self.config.build_folder, self.path))
+                print color_green.format('Folder ' + self.path + ' copied')
             else:
-                shutil.copy(self.path_from, build_folder + self.path_to)
-                print color_green.format('File ' + self.path_from + ' copied')
+                shutil.copy(self.path, os.path.join(self.config.build_folder, self.path))
+                print color_green.format('File ' + self.path + ' copied')
         else:
-            print color_red.format('File ' + self.path_from + ' not found')
+            print color_red.format('File ' + self.path + ' not found')
 
 class Build:
-
-    def __init__(self, config, result, build):
+    def __init__(self, config, path):
         self.config = config
-        self.result = copy.deepcopy(result)
-
-        self.path = build['path']
-        self.raw_contents = ''
-        self.contents = []
-        if 'contents' in build:
-            self.raw_contents = build['contents']
-            self.parse_content(build['contents'])
-
-    def parse_content(self, contents):
-        for content in contents:
-            self.contents.append(Build(self.config, self.result, content))
+        self.path = path
 
     def build(self):
         if os.path.isdir(self.path):
             self.build_folder()
         else:
             self.build_file()
+            #TODO SAVE THE FILE GENERATED
 
     def build_folder(self):
         paths = os.listdir(self.path)
-        for path in paths:
-            new_build = Build(self.config, self.result, {'path': os.path.join(self.path, path)})
+        for file_path in paths:
+            new_build = Build(self.config, os.path.join(self.path, file_path))
             new_build.build()
 
     def build_file(self):
         if os.path.exists(self.path):
-            file_content = open(self.path, 'r').read()
-            file_format = os.path.splitext(self.path)[1]
-
-            self.result.insert(file_content, file_format)
-
-            if self.contents:
-                for content in self.contents:
-                    new_build = {
-                        'path': content.path
-                    }
-                    if content.contents:
-                        new_build['contents'] = content.raw_contents
-                    Build(self.config, self.result, new_build).build()
-            else:
-                self.result.save(self.path)
+            builder = FileBuilder(self.path, self.config)
+            file_content = builder.build()
+            self.save(file_content)
         else:
             print color_red.format('File ' + self.path + ' not found')
+            return None
 
-class ResultFile:
-
-    def __init__(self, config):
-        self.content = ''
-        self.config = config
-
-    def insert(self, new_content, file_format):
-        if self.content:
-            replace = re.findall(self.config.start_tag + ' (\w+) ' + self.config.end_tag, self.content)
-            for tag in replace:
-                delimiter = self.config.start_tag+' '+tag+' '+self.config.end_tag
-
-                match = re.search(delimiter+'(.*)'+delimiter, new_content, re.S)
-                if match:
-                    text = match.group(1)
-
-                    if file_format == '.md':
-                        text = markdown2.markdown(text).encode('utf8', 'ignore').strip()
-
-                    self.content = re.sub(delimiter, text, self.content)
-        else:
-            self.content = new_content
-
-    def save(self, current_path):
-        file_name = os.path.splitext(current_path)[0] + '.html'
-        result_path = self.config.build_folder + file_name
+    def save(self, content):
+        file_name = os.path.splitext(self.path)[0] + '.html'
+        result_path = os.path.join(self.config.build_folder, file_name)
         if not os.path.exists(os.path.dirname(result_path)):
             os.makedirs(os.path.dirname(result_path))
 
         result = open(result_path, 'w')
-        result.write(self.content)
+        result.write(content)
         result.close()
 
-        print color_green.format('File ' + file_name + ' builded')
+        print color_green.format('File ' + self.path + ' builded')
+
+class FileBuilder:
+    def __init__(self, path, config):
+        self.path = path
+        self.config = config
+
+    def build(self):
+        file_content = open(self.path, 'r').read()
+
+        file_format = os.path.splitext(self.path)[1]
+        if file_format == '.md':
+            file_content = markdown2.markdown(file_content).encode('utf8', 'ignore').strip()
+
+        template_path = self.get_template(file_content)
+        if template_path:
+            if os.path.exists(template_path):
+                file_builder = FileBuilder(template_path, self.config)
+                template_content = file_builder.build()
+
+                return self.replace_content(template_content, file_content)
+            else:
+                print color_red.format('Template ' + template_path + ' not found')
+                return None
+        else:
+            return file_content
+
+    def get_template(self, file_content):
+        match = re.search("<!-- pwb:extends\('(.+)'\) -->", file_content)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+    def replace_content(self, template_content, file_content):
+        contents = re.findall("<!-- pwb:content\('(.+)'\) -->", template_content)
+
+        for content in contents:
+            match = re.search("<!-- pwb:section\('"+content+"'\) -->\s*(.*?)\s*<!-- pwb:end-section -->", file_content, re.S)
+            if match:
+                replace = match.group(1)
+            else:
+                replace = ''
+
+            template_content = re.sub("<!-- pwb:content\('" + content + "'\) -->", replace, template_content)
 
 
-builder = WebsiteBuilder()
-builder.build()
+        return template_content
+
+website_builder = WebsiteBuilder()
+website_builder.build()
